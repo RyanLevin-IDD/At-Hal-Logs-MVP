bb74d93be3889abae0fbac0dc128c7a1e8e6e93a
Ryan Levin <ryan@idd.co.il>
2025-10-29 16:44:39 +0200

    Delete At Hal Logs directory

--- At Hal Logs/At Hal Logs/LogEntry.cs
+++
@@ -1,8 +0,0 @@
-﻿public class LogEntry
-{
-    public DateTime Time { get; set; }
-    public string Ip { get; set; }
-    public string Method { get; set; }
-    public string Path { get; set; }
-    public string Status { get; set; }
-}
